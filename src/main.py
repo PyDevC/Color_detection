3a6e4cf79314658453147bf8d1e543a188795bc9
PyDevC <anuskcse@gmail.com>
2024-10-06 01:22:49 +0000

    Updataed

--- src/main.py
+++ src/main.py
@@ -3,9 +3,22 @@ import cv2
 import sys
 
 if __name__ == "__main__":
-    for i in sys.argv[1:]:
-        img = cv2.imread(f'test/images/{i}')
-        hsvmethod(img)
-        cv2.imshow("",img)
+    mode = sys.argv[3]
+    color = sys.argv[1]
+    i = sys.argv[2]
+    
+    if mode.lower() == '-i':
+        img = cv2.imread(f"../test/images/{i}")
+        hsvmethod(img,color)
+        cv2.imshow(i,img)
         if cv2.waitKey(0):
             cv2.destroyAllWindows()
+    elif mode.lower() == '-v':
+        cam = cv2.VideoCapture(0)
+        while True:
+            _, frame = cam.read()
+            hsvmethod(frame, color)
+            cv2.imshow("Camear", frame)
+            if cv2.waitKey(0):
+                cv2.destroyAllWindows()
+                break
--- src/masking.py
+++ src/masking.py
@@ -1,97 +1,77 @@
 import numpy as np
 import cv2
 
-def hsvmethod(frame):
-	#converting bgr to hsv 
-    
+def hsvmethod(frame, color):    
     hsvFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
+    kernel = np.ones((5, 5), "uint8")
+    if color.lower() == 'red':
+        reddetect(frame, hsvFrame, kernel)
+    elif color.lower() == 'blue':
+        bluedetect(frame, hsvFrame, kernel)
+    elif color.lower() == 'green':
+        greendetect(frame, hsvFrame, kernel)
+    elif color.lower() == 'all':
+        all(frame, hsvFrame, kernel)
+    
+            
+def reddetect(frame, hsvFrame, kernel):
+    red_lower = np.array([0, 100, 100], np.uint8)
+    red_upper = np.array([10, 255, 255], np.uint8)
+    red_mask = cv2.inRange(hsvFrame, red_lower, red_upper)
 
-	# Set range for red color and 
-	# define mask 
-    red_lower = np.array([136, 87, 111], np.uint8)
-    red_upper = np.array([180, 255, 255], np.uint8)
-    red_mask = cv2.inRange(hsvFrame, red_lower, red_upper) 
+    red_mask = cv2.dilate(red_mask, kernel) 
+    res_red = cv2.bitwise_and(frame, frame, mask = red_mask)
 
-	# Set range for green color and 
-	# define mask 
+    contours, hierarchy = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
+	
+    for pic, contour in enumerate(contours): 
+        area = cv2.contourArea(contour) 
+        if(area > 300): 
+            x, y, w, h = cv2.boundingRect(contour) 
+            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2) 
+			
+            cv2.putText(frame, "Red Colour", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255))
+    
+    
+            
+def greendetect(frame, hsvFrame, kernel):
     green_lower = np.array([25, 52, 72], np.uint8) 
     green_upper = np.array([102, 255, 255], np.uint8) 
-    green_mask = cv2.inRange(hsvFrame, green_lower, green_upper) 
+    green_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
 
-	# Set range for blue color and 
-	# define mask 
+    green_mask = cv2.dilate(green_mask, kernel) 
+    res_green = cv2.bitwise_and(frame, frame, mask = green_mask) 
+
+    contours, hierarchy = cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
+	
+    for pic, contour in enumerate(contours): 
+        area = cv2.contourArea(contour) 
+        if(area > 300): 
+            x, y, w, h = cv2.boundingRect(contour) 
+            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2) 
+			
+            cv2.putText(frame, "Green Colour", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0)) 
+
+
+def bluedetect(frame, hsvFrame, kernel):
     blue_lower = np.array([94, 80, 2], np.uint8) 
     blue_upper = np.array([120, 255, 255], np.uint8) 
-    blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper) 
-	
-	# Morphological Transform, Dilation 
-	# for each color and bitwise_and operator 
-	# between frame and mask determines 
-	# to detect only that particular color 
-    kernel = np.ones((5, 5), "uint8") 
-	
-	# For red color 
-    red_mask = cv2.dilate(red_mask, kernel) 
-    res_red = cv2.bitwise_and(frame, frame, 
-							mask = red_mask) 
-	
-	# For green color 
-    green_mask = cv2.dilate(green_mask, kernel) 
-    res_green = cv2.bitwise_and(frame, frame, 
-								mask = green_mask) 
-	
-	# For blue color 
+    blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper)
+
     blue_mask = cv2.dilate(blue_mask, kernel) 
-    res_blue = cv2.bitwise_and(frame, frame, 
-							mask = blue_mask) 
+    res_blue = cv2.bitwise_and(frame, frame, mask = blue_mask) 
 
-	# Creating contour to track red color 
-    contours, hierarchy = cv2.findContours(red_mask, 
-										cv2.RETR_TREE, 
-										cv2.CHAIN_APPROX_SIMPLE) 
-	
+    contours, hierarchy = cv2.findContours(blue_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
+    
     for pic, contour in enumerate(contours): 
         area = cv2.contourArea(contour) 
         if(area > 300): 
             x, y, w, h = cv2.boundingRect(contour) 
-            frame = cv2.rectangle(frame, (x, y), 
-									(x + w, y + h), 
-									(0, 0, 255), 2) 
+            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2) 
 			
-            cv2.putText(frame, "Red Colour", (x, y), 
-						cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
-						(0, 0, 255))	 
+            cv2.putText(frame, "Blue Colour", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0)) 
 
-	# Creating contour to track green color 
-    contours, hierarchy = cv2.findContours(green_mask, 
-										cv2.RETR_TREE, 
-										cv2.CHAIN_APPROX_SIMPLE) 
-	
-    for pic, contour in enumerate(contours): 
-        area = cv2.contourArea(contour) 
-        if(area > 300): 
-            x, y, w, h = cv2.boundingRect(contour) 
-            frame = cv2.rectangle(frame, (x, y), 
-									(x + w, y + h), 
-									(0, 255, 0), 2) 
-			
-            cv2.putText(frame, "Green Colour", (x, y), 
-						cv2.FONT_HERSHEY_SIMPLEX, 
-						1.0, (0, 255, 0)) 
-
-	# Creating contour to track blue color 
-    contours, hierarchy = cv2.findContours(blue_mask, 
-										cv2.RETR_TREE, 
-										cv2.CHAIN_APPROX_SIMPLE) 
-    for pic, contour in enumerate(contours): 
-        area = cv2.contourArea(contour) 
-        if(area > 300): 
-            x, y, w, h = cv2.boundingRect(contour) 
-            frame = cv2.rectangle(frame, (x, y), 
-									(x + w, y + h), 
-									(255, 0, 0), 2) 
-			
-            cv2.putText(frame, "Blue Colour", (x, y), 
-						cv2.FONT_HERSHEY_SIMPLEX, 
-						1.0, (255, 0, 0)) 
-            
+def all(frame, hsvFrame, kernel):
+    reddetect(frame, hsvFrame, kernel)
+    bluedetect(frame, hsvFrame, kernel)
+    greendetect(frame, hsvFrame, kernel)
